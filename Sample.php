b96135e4809dcf6e934701f5369bd006d8a5c5c3
davi-damasceno <davi.damasceno@pyyne.com>
2023-06-22 20:02:37 -0400

    Adding a style sheet

--- Sample.php
+++ Sample.php
@@ -1,18 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TMG-CODING-EXERCISE-PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 <?php
-$form = new Form();
+require 'Form.php';
+require 'Inputs/TextInput.php';
 
+$form = new Form();
 $form->addInput(new TextInput("firstname", "First Name", "Bruce"));
 $form->addInput(new TextInput("lastname", "Last Name", "Wayne"));
 
-if ($_SERVER['METHOD']=="POST") {
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     if ($form->validate()) {
         // display user info
         $firstName = $form->getValue("firstname");
         $lastName = $form->getValue("lastname");
-        echo $firstName." ".$lastName;
+        echo $firstName . " " . $lastName;
     } else {
         $form->display();
     }
 } else {
     $form->display();
 }
+?>    
+</body>
+</html>
